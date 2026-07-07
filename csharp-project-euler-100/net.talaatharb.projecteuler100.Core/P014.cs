f8da67e446809f6a1835e7d08591266caa1e3f81
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-07 18:21:39 +0300

    solve c# p014

--- csharp-project-euler-100/net.talaatharb.projecteuler100.Core/P014.cs
+++ csharp-project-euler-100/net.talaatharb.projecteuler100.Core/P014.cs
@@ -23,7 +23,39 @@ namespace net.talaatharb.projecteuler100.Core
         public override long Solve()
         {
             problemNumber = 14;
-            return 837799;
+            int limit = 1000000;
+            int maxCount = 0;
+            int maxNumber = 0;
+            Dictionary<int, int> list = new();
+            for (int i = 1; i < limit; i++)
+            {
+                int count = 0;
+                long n = i;
+                while(n != 1)
+                {
+                    if(n % 2 == 0)
+                    {
+                        n /= 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                    count++;
+                    if (list.ContainsKey((int)n))
+                    {
+                        count += list[(int)n];
+                        break;
+                    }
+                }
+                list[i] = count;
+                if(count > maxCount)
+                {
+                    maxCount = count;
+                    maxNumber = i;
+                }
+            }
+            return maxNumber;
         }
     }
 }
