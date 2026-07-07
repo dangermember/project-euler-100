eb90770906be923c9bb1e2dc896622d9bde7f7f6
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-07 19:35:12 +0300

    c# p017 solution

--- csharp-project-euler-100/net.talaatharb.projecteuler100.Core/P017.cs
+++ csharp-project-euler-100/net.talaatharb.projecteuler100.Core/P017.cs
@@ -19,11 +19,49 @@ namespace net.talaatharb.projecteuler100.Core
         {
             new P017().Run();
         }
+        List<string> ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        List<string> teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+        List<string> tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+        List<string> hundreds = ["onehundred", "twohundred", "threehundred", "fourhundred", "fivehundred", "sixhundred", "sevenhundred", "eighthundred", "ninehundred"];
+        private string NumberToText(int number)
+        {
+            List<string> output = [];
+            int hundredsValue = number / 100;
+            int tensValue = number / 10 % 10;
+            int onesValue = number % 10;
+            if(number == 1000)
+            {
+                return "onethousand";
+            }
+            if (hundredsValue > 0)
+            {
+                output.Add(hundreds[hundredsValue - 1]);
+            }
+            if (tensValue >= 2)
+            {
+                output.Add(tens[tensValue - 2] + (onesValue > 0 ? ones[onesValue - 1] : ""));
+            }
+            else if (tensValue == 1)
+            {
+                output.Add(teens[onesValue]);
+            }
+            else if (tensValue == 0 && onesValue > 0) {
+                output.Add(ones[onesValue-1]);
+            }
+
+            // Implementation for converting number to text
+            return string.Join("and", output);
+        }
 
         public override long Solve()
         {
             problemNumber = 17;
-            return 21124;
+            long sum = 0;
+            for(int i = 1; i <= 1000; i++)
+            {
+                sum+= NumberToText(i).Length;
+            }
+            return sum;
         }
     }
 }
